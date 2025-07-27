affa55ff9d581e99624d3ded85b0850b51e23e9f
binikiya <biniyamkiya3682@gmail.com>
2025-07-27 12:05:31 +0300

    Graphql

--- crm/models.py
+++ crm/models.py
@@ -6,11 +6,17 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
